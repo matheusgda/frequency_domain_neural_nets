b724f92959254b02f17f871cb08558ceaeff012a
Matheus Guedes de Andrade <matheusgandrade@poli.ufrj.br>
2020-11-18 19:28:27 -0300

    elegant training script for CIFAR-10

--- CIFAR_10.py
+++ CIFAR_10.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-NUM_TRAIN = 640
-NUM_VAL = 128
-BATCH_SIZE = 64
-
 import torch
 import torchvision
 
@@ -16,45 +11,49 @@ import fdnn
 import utils
 import matplotlib.pyplot as plt
 
-
-
 import sys
+
+NUM_TRAIN = 256
+NUM_VAL = 128
+BATCH_SIZE = 128
+NUM_TEST = 128
 
 CIFAR10_PATH = '/home/revz/Development/neural_nets/assignment2/cs682/datasets'
 if len(sys.argv) > 1:
     CIFAR10_PATH = sys.argv[1]
 
-
-
-device = torch.device("cuda:0")
-dtype = torch.cfloat
-
-N, C, H, W, K = 64, 3, 32, 32, 10
+N, C, H, W, K = BATCH_SIZE, 3, 32, 32, 10
 
 transform = T.Compose([
                 T.ToTensor(),
                 T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
             ])
 
-# Create random input and output data
+
 cifar10_train = torchvision.datasets.CIFAR10(
     CIFAR10_PATH,
     train=True, download=True, transform=transform)
 
-cifar10_validation = torchvision.datasets.CIFAR10(
+cifar10_test = torchvision.datasets.CIFAR10(
     CIFAR10_PATH,
     train=False, download=True, transform=transform)
 
 train_loader = DataLoader(cifar10_train, batch_size=BATCH_SIZE, 
                           sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN)))
 
-val_loader = DataLoader(cifar10_validation, batch_size=BATCH_SIZE, 
-                          sampler=sampler.SubsetRandomSampler(range(NUM_VAL)))
+val_loader = DataLoader(
+    cifar10_train, batch_size=BATCH_SIZE,
+    sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN, NUM_TRAIN + NUM_VAL)))
 
+test_loader = DataLoader(
+    cifar10_test, batch_size=BATCH_SIZE,
+    sampler=sampler.SubsetRandomSampler(range(NUM_TEST)))
+
+device = torch.device("cuda:0")
 
 dims = (N, H, W, C, 1)
-p_num_filters = (1, 32, 32, 32)
-m_num_filters = (16, 16, 16)
+p_num_filters = (1, 3, 3, 3)
+m_num_filters = (3, 3, 3)
 preserved_dim = 3
 initializer = fdnn.random_complex_weight_initializer
 hadamard_initializer = fdnn.random_hadamard_filter_initializer
@@ -74,18 +73,19 @@ utils.print_param_counter(model)
 preprocess = fdnn.FourierPreprocess()
 
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, betas=(0.9,0.99))
+optimizer = torch.optim.Adam(
+    model.parameters(), lr=1e-4, betas=(0.9,0.99), weight_decay=0)
 
-num_epochs = 50
+num_epochs = 10
 
 loss, accuracy = utils.trainer(
     preprocess,
-    model, num_epochs, K, train_loader, val_loader,criterion, optimizer, device)
+    model, num_epochs, K, train_loader, val_loader, NUM_VAL, 
+    criterion, optimizer, device)
 
-plot = True
+test_accuracy = utils.evaluate(preprocess, model, test_loader, NUM_TEST, device)
 
-
-torch.save(model.state_dict(), "FDNN_CIFAR10_1M_Overffited.model")
+# torch.save(model.state_dict(), "FDNN_CIFAR10_1M_Overffited_RELU.model")
 
 plt.plot(range(len(loss)), loss)
 plt.ylabel("Loss")
@@ -99,3 +99,5 @@ plt.ylabel("Accuracy")
 plt.xlabel("Epoch")
 plt.savefig("accuracy.png")
 plt.show()
+
+print("Accuracy on test set: {}.".format(test_accuracy))
